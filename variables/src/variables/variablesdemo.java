f13ad80ca3a8193ea12782cf62489e199381966e
unknown <20000999@H03-49-16-HPE.glasgowclyde.ac.uk>
2015-10-09 15:53:56 +0100

    added variable files

---
+++ variables/src/variables/variablesdemo.java
@@ -0,0 +1,33 @@
+package variables;
+
+public class variablesdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int numberOfStudents;
+String name;
+double balance;
+boolean isFinished;
+char firstInitial, lastInitial;
+
+	
+	
+		
+		numberOfStudents=21;
+		name="scott";
+		balance = 102.50;
+		isFinished = true;
+		firstInitial='J';
+		lastInitial='W';
+		
+		
+				System.out.print(name +" " + firstInitial );
+				
+				double rate, interest,principal;
+				rate = 0.7;
+				principal = 300;
+				interest = rate * principal;
+				System.out.println(interest);
+	}
+
+}
